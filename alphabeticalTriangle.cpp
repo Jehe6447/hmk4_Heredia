abd029d75b1fe1b016481d08790c6a2ddcade131
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2023-02-17 13:03:29 -0700

    Create alphabeticalTriangle.cpp

---
+++ alphabeticalTriangle.cpp
@@ -0,0 +1,35 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input;
+    // declare variables
+    int idx = 0;
+    char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    
+    // prompt user to input height
+    cout << "Enter the height:" << endl;
+    cin >> input;
+    // input validation
+    if (input <= 0){
+        cout << "Invalid input." << endl;
+        return 0;
+    }
+    
+    // loop to compute alphabetic triangle
+    for(int i = 1; i <= input; i++){
+        int line = (i*2)-1;
+        for(int j = 0; j < line; j++){
+            if (idx > 25){
+                idx = 0;
+            }
+            cout << alphabet[idx];
+            idx++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
